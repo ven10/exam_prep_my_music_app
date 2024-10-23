f34bcb066c33e1b11c40137026b4e6c488a174a5
Венелин Христов <v.hristov@rg-plovdiv.dep>
2024-10-23 17:29:51 +0300

    added: album details

--- exam_prep_my_music_app/albums/urls.py
+++ exam_prep_my_music_app/albums/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.views.generic import DetailView
+
+from exam_prep_my_music_app.albums.views import DetailAlbumView
 from exam_prep_my_music_app.albums.views import CreateAlbumView
 
 urlpatterns = (
     path("add/", CreateAlbumView.as_view(), name="create_album"),
+    path("<int:pk>/details/", DetailAlbumView.as_view(), name="details_album"),
 
 )
--- exam_prep_my_music_app/albums/views.py
+++ exam_prep_my_music_app/albums/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.template.base import kwarg_re
-from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView
 from exam_prep_my_music_app.albums.models import Album
 from exam_prep_my_music_app.web.views import get_profile
 
@@ -11,15 +12,8 @@ class CreateAlbumView(CreateView):
     queryset = Album.objects.all()
     fields = ('name', 'artist_name', 'genre', 'description', 'image_url', 'price')
     template_name = "albums/album-add.html"
+    success_url = reverse_lazy("index")
 
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-
-        kwargs["data"] = {
-            **kwargs["data"],
-            "profile": get_profile()
-        }
-        return kwargs
 
     def get_form(self, form_class=None):
         form = super().get_form(form_class=form_class)
@@ -30,6 +24,17 @@ class CreateAlbumView(CreateView):
         form.fields["image_url"].widget.attrs["placeholder"] = "Image URL"
         form.fields["price"].widget.attrs["placeholder"] = "Price"
 
-
         return form
 
+    # TODO: Check for better solution.Working OK
+    def form_valid(self, form):
+        # instance = form.save(commit=False)
+        # instance.owner = get_profile()
+        form.instance.owner_id = get_profile().pk
+
+        return super().form_valid(form)
+
+
+class DetailAlbumView(DetailView):
+    queryset = Album.objects.all()
+    template_name = "albums/album-details.html"
